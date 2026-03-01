9c73e6d024ba755deb7327fd27144a5763173087
thienanh411 <ldthienanh@gmail.com>
2026-03-02 00:37:00 +0700

    delete old be

--- Frontend/src/components/common/footer/Footer.tsx
+++
@@ -1,6 +0,0 @@
-import FooterView from './FooterView'
-
-export default function Footer() {
-  // container kept for future logic
-  return <FooterView />
-}
--- Frontend/src/components/common/header/Header.tsx
+++
@@ -1,6 +0,0 @@
-import HeaderView from './HeaderView'
-
-export default function Header() {
-  // currently no logic; container kept for future state/props
-  return <HeaderView />
-}
