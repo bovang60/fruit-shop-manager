a6d18f22836b67b801df138a5d9877ed23459039
thienanh411 <ldthienanh@gmail.com>
2026-03-28 21:35:49 +0700

    .

--- Backend/src/main/java/com/fruitshop/backend/model/Product.java
+++ Backend/src/main/java/com/fruitshop/backend/model/Product.java
@@ -18,10 +18,6 @@ public class Product {
     private Integer productId;
 
     @ManyToOne
-    @JoinColumn(name = "shop_id", foreignKey = @ForeignKey(name = "FK_products_shop"))
-    private Shop shop;
-
-    @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
 
